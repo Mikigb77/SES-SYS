81f637c0ddbeba2898afff03b92f291deb1f8b70
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-01 22:11:11 +0200

    Main

--- public/index.php
+++ public/index.php
@@ -10,8 +10,14 @@ for ($i = 0; $i < 20; ++$i)
     echo "<br>";
 ?>
 <div class="MySection" id="presentation">
-    <div style="max-width: 50%;" style="display:inline-block ;">
-        <h1>"Lo complicado es alcanzar lo sencillo"</h1>
+    <div style="display:inline-block ;">
+        <font size="+3">
+            <img style="width: 50%; height:50%; border-radius: 10%; float:right; margin-top:20px; margin-bottom:20px; margin-left: 20px;; margin-right:20px;" src="/media/images/dad_vang.jpg">
+            <p><strong>
+                    <h1>"Lo complicado es alcanzar lo sencillo"</h1>
+                </strong>
+            </p>
+            <p>“Un consultor de estructuras debe siempre tener una respuesta ante las posibles necesidades que genere el proyecto”</p>
+        </font>
     </div>
-
 </div>
