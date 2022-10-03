48c867f4488bb7021161ecdbdb5dcbebe3ae8eb6
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-03 13:05:04 +0200

    Main

--- public/index.php
+++ public/index.php
@@ -6,18 +6,21 @@ require_once(__DIR__ . "/../layout/header.php");
     Video Unsuported!!!
 </video>
 <?
-for ($i = 0; $i < 20; ++$i)
+for ($i = 0; $i < 10; ++$i)
     echo "<br>";
 ?>
 <div class="MySection" id="presentation">
-    <div style="display:inline-block ;">
+    <div style="display:inline-block ; padding: 20px;">
         <font size="+3">
-            <img style="width: 50%; height:50%; border-radius: 10%; float:right; margin-top:20px; margin-bottom:20px; margin-left: 20px;; margin-right:20px;" src="/media/images/dad_vang.jpg">
+            <img style="width: 50%; height:50%; border-radius: 10%; float:right; margin-left:10px;" src="/media/images/dad_vang.jpg">
             <p><strong>
                     <h1>"Lo complicado es alcanzar lo sencillo"</h1>
                 </strong>
             </p>
             <p>“Un consultor de estructuras debe siempre tener una respuesta ante las posibles necesidades que genere el proyecto”</p>
         </font>
+        <p style="text-align:right ;"><i>- Joan Francesc Garcia Beltrán -</i></p>
+        <img style="width:10% ; margin-top:30px;" src="/media/images/ses-steel.png">
+
     </div>
 </div>
