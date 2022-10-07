bed0a41fe13fed1317bbf293d507c6533bd6dc13
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-07 21:27:57 +0200

    Main

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'mique/ses-sys',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '733e33e7d460793327647cd0fdb0d4268e8c9b98',
+        'reference' => 'db04affffe8303727dcf6ee8e366bc882daf904f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'mique/ses-sys' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '733e33e7d460793327647cd0fdb0d4268e8c9b98',
+            'reference' => 'db04affffe8303727dcf6ee8e366bc882daf904f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
