c23afc5fdc46ac67b97a6fc4b7b25d2af50d09b9
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-07 21:57:43 +0200

    test
    Signed-off-by: Mikigb77 <miquelgarcia.alu@gmail.com>

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?
-require_once(__DIR__ . "/public/index.php");
+//require_once(__DIR__ . "/public/index.php");
+echo '<pre>';
+var_dump($_SERVER['REQUEST_URI']);
+echo '</pre>';
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'mique/ses-sys',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b1883bd2b74465933acd13befb8622fdb7d806db',
+        'reference' => 'ce9363725f9b67c555eeb820ea280b32d36c1d9a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'mique/ses-sys' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b1883bd2b74465933acd13befb8622fdb7d806db',
+            'reference' => 'ce9363725f9b67c555eeb820ea280b32d36c1d9a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
