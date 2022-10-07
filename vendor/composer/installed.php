1337ce9d87d7a634e08a8a84fc3795273cc08bbb
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-07 21:59:37 +0200

    c
    Signed-off-by: Mikigb77 <miquelgarcia.alu@gmail.com>

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'mique/ses-sys',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ce9363725f9b67c555eeb820ea280b32d36c1d9a',
+        'reference' => '3f99a64bd374c53033caae24d4b6d4166ad9e438',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'mique/ses-sys' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ce9363725f9b67c555eeb820ea280b32d36c1d9a',
+            'reference' => '3f99a64bd374c53033caae24d4b6d4166ad9e438',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
