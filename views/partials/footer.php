731fdc48e08a70e672ce4c78ea626946f7f4194b
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-11-22 12:54:45 +0100

    c
    Signed-off-by: Mikigb77 <miquelgarcia.alu@gmail.com>

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -5,7 +5,6 @@ for ($i = 0; $i < 5; ++$i)
 <article></article>
 <div class="MyFooter container-fluid">
     <footer>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam fugit, modi provident ipsum hic voluptas eveniet ex exercitationem odio labore voluptates asperiores dolore, dicta necessitatibus, fugiat porro est non maiores.</p>
 
     </footer>
 </div>
