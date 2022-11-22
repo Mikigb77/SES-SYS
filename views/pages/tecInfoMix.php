f057a9174dee50d6d6378c08ae49a8d2935caa21
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-11-22 19:02:05 +0100

    c
    Signed-off-by: Mikigb77 <miquelgarcia.alu@gmail.com>

--- views/pages/tecInfoMix.php
+++ views/pages/tecInfoMix.php
@@ -1,8 +1,45 @@
 <div class="MySection container">
     <div class="row">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, error! Vero nostrum, quam ipsum, totam reiciendis obcaecati accusamus quisquam, numquam repudiandae cum nulla minus omnis adipisci! Reiciendis voluptas maiores quibusdam!</p>
-        <p>Dolores architecto obcaecati harum, aspernatur dolor reiciendis illum nulla accusantium! Magnam, ipsum aut vitae autem pariatur recusandae magni. Asperiores, quam! Voluptates odio quo deserunt pariatur esse aperiam ab ea recusandae?</p>
-        <p>Eaque blanditiis, ex pariatur facilis officia a vitae cum inventore quod, magni animi ipsum laudantium modi! Esse rerum, nisi ab doloribus recusandae dolorum aspernatur, deleniti tempore, magni aliquam vitae reprehenderit!</p>
-        <p>Earum doloremque dicta laudantium itaque ipsum ex numquam fugiat, iusto suscipit ratione alias sint rerum cupiditate tempore. Numquam, repellendus quaerat minima dolores, quia laboriosam quae totam reiciendis officiis saepe iste.</p>
+        <div class="col-md-12">
+            <h1 style="text-align:center;">Información Técnica</h1>
+            <hr>
+            <h2 style="text-align:center;">Sistema Mixto</h2>
+            <br>
+
+            <div>
+                <img src="/media/images/ses-steel.png" alt="" style="width:25%; float:left; margin-right:2%; border-radius:10%">
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga asperiores debitis reiciendis necessitatibus labore similique temporibus, incidunt dolor quam? Quasi deleniti et delectus quod quaerat maiores tempora! Tempore, voluptatem veniam?</p>
+                <p>Voluptatem sit quidem consequuntur nihil. Eius quis rerum incidunt illum doloremque dignissimos libero quos? Inventore quae dolore impedit totam, laborum accusamus animi ipsam consectetur libero atque dicta veniam magnam deserunt?</p>
+                <p>Neque mollitia nulla consectetur vitae error maiores quis eum temporibus unde? Maiores aspernatur nulla sequi eum ab commodi molestias beatae possimus maxime saepe culpa, fuga qui libero, repellat consequatur eaque!</p>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br><br><br>
+
+<div class="MySection container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 style="text-align:center;"><b>Ficha Técnica</b></h2>
+            <hr>
+        </div>
+        <div class="col-md-6">
+            <img src="/media/images/joan equip.jpg" alt="..." style="width:65%;">
+        </div>
+        <div class="col-md-6">
+
+            <div>
+                <a href="/media/files/SES_mixto_ficha técnica.pdf" target="_blank">
+                    <img src="/media/images/SES_mixto_info.jpg" alt="Ficha Técnica SES Mixto" style="width:65%;">
+                </a>
+            </div>
+            <br>
+            <div style="text-align:center;">
+                <a href="/media/files/SES_mixto_ficha técnica.pdf" target="_blank">
+                    <button type="button" class="btn btn-outline-dark"><b>Descargar</b></button>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -3,8 +3,41 @@ for ($i = 0; $i < 5; ++$i)
     echo '<br>';
 ?>
 <article></article>
-<div class="MyFooter container-fluid">
-    <footer>
 
-    </footer>
-</div>
+<footer class="MyFooter container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div>
+                <img src="/media/images/icon-ses.png" alt="JFG Consultors" style="width:50%;">
+                <br>
+                <a href="/politica-de-privacidad">Política de privacidad</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div style="text-align:center;">
+                <a href="/">
+                    <img src="/media/images/SES Steel Embed System - Foto expo.jpg" alt="..." style="width:75%;">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+</footer>
