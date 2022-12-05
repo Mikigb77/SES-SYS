24710d8489be6dcdf87c5837486d19a60f4de85a
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-12-05 20:55:23 +0100

    c
    Signed-off-by: Mikigb77 <miquelgarcia.alu@gmail.com>

--- views/pages/tecInfo.php
+++ views/pages/tecInfo.php
@@ -43,32 +43,6 @@
                 </a>
             </div>
         </div>
-        <div class="col-md-4">
-            <form action="">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input name="message" type="text" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
+
     </div>
 </div>
-
-<?
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $to = urldecode($_GET['email']);
-    $tema = 'TEST';
-    $message = urldecode($_GET['message']);
-    mail($to, $tema, $message);
-}
-?>
